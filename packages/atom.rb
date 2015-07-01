35e75db32cc30f1ceab652ab42e0d045c761d271
Chris Oakley <CAOakleyII@gmail.com>
2015-07-01 01:46:24 -0400

    added atom text editor

---
+++ packages/atom.rb
@@ -0,0 +1,19 @@
+require 'package'
+
+class atom < Package
+  version '1.0.0'
+  source_url 'https://github.com/atom/atom/archive/v1.0.0.tar.gz'
+  source_sha1 '66fab6125ccf2feb2316dd92686ff7c0f96a9780'
+
+  depends_on 'gcc'
+  depends_on 'buildessential'
+  depends_on 'nodejs'
+
+  def self.build
+    system "script/build"
+  end
+
+  def self.install
+    system "sudo script/grunt install --install-dir #{CREW_DEST_DIR}"
+  end
+end
